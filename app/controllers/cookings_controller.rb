726c75eb85fcef3c3c6be74c17b541a7c4bfb1a5
muromoto-y <6610010@gmail.com>
2020-09-03 00:13:41 +0900

    index原型

--- app/controllers/cookings_controller.rb
+++ app/controllers/cookings_controller.rb
@@ -1,2 +1,27 @@
 class CookingsController < ApplicationController
+
+  def index
+    @cookings = Cooking.all.order(created_at: :desc)
+  end
+
+  def new
+    @cooking = CookingsTag.new
+  end
+
+  def create
+    @cooking = CookingsTag.new(cooking_params)
+    if @cooking.valid?
+      @cooking.save
+      return redirect_to root_path
+    else
+      render "new"
+    end
+  end
+
+  private
+
+  def cooking_params
+    params.require(:cookings_tag).permit(:cooking_name, :text, :material, :name).merge(user_id: current_user.id)
+  end
+
 end
--- app/forms/cookings_tag.rb
+++ app/forms/cookings_tag.rb
@@ -3,17 +3,18 @@ class CookingsTag
   include ActiveModel::Model
   attr_accessor :cooking_name, :text, :material, :user_id, :name,
 
-  with_options presence: true do
-    validates :cooking_name                
-    validates :image 
-    validates :text
-    validates :material
-    validates :name
-  end
+  # with_options presence: true do
+  #   validates :cooking_name
+  #   validates :image 
+  #   validates :text
+  #   validates :material
+  #   validates :name
+  # end
 
   def save
-    cooking = Cooking.create(cooking_name: cooking_name, text: text, material: material, )
-    tag = Tag.create(name: name)
+    cooking = Cooking.create(cooking_name: cooking_name, text: text, material: material)
+    tag = Tag.where(name: name).first_or_initialize
+    tag.save
 
     CookingTagRelation.create(cooking_id: cooking.id, tag_id: tag.id)
   end
