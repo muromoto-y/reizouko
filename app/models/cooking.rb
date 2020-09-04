78c4cad68b4b8d3aeec3bfc172bc502191df2d88
muromoto-y <6610010@gmail.com>
2020-09-04 16:00:03 +0900

    topビューかいし

--- app/models/cooking.rb
+++ app/models/cooking.rb
@@ -2,7 +2,7 @@ class Cooking < ApplicationRecord
 
   # has_many :cooking_tag_relations
   # has_many :tags, through: :cooking_tag_relations
-  # has_one_attached :image
+  has_one_attached :image
 
   # with_options presence: true do
   #   validates :cooking_name                
--- db/migrate/20200902073628_devise_create_users.rb
+++ db/migrate/20200902073628_devise_create_users.rb
@@ -6,7 +6,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       ## Database authenticatable
       t.string :nick_name, null: false
       t.string :email,    null: false, default: ""
-      t.string :password, null: false, default: ""
+      t.string :encrypted_password, null: false, default: ""
 
       ## Recoverable
       t.string   :reset_password_token
