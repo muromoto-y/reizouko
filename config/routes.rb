759194361a847487d4345ff12710622ad88adf97
muromoto-y <6610010@gmail.com>
2020-09-09 17:59:57 +0900

    検索機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "cookings#index"
   resources :cookings
-  collection do
-    get 'search'
-  end
+  # collection do
+  #   get 'search'
+  # end
 end
